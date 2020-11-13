456bab68767964d64eab19fbd65f00c9991fcb7e
coleee1205 <72029785+coleee1205@users.noreply.github.com>
2020-11-12 23:53:25 -0600

    Update ODEUnitTest.py

--- tests/ODEUnitTest.py
+++ tests/ODEUnitTest.py
@@ -20,7 +20,7 @@ class TestODEs(unittest.TestCase):
         '''
         h = 600 #Time Step - 600 days
         k = 2*h #Time Step - half days
-        y_init = np.array([0.99999, 0.00001, 0]) #Initial Condition
+        SIR_init = np.array([0.99999, 0.00001, 0]) #Initial Condition
         tspan = np.linspace(0, 600, k + 2) #600 days, halfday-steps + 1 extra
         
         b = .30 #Infection Rate
@@ -50,26 +50,26 @@ class TestODEs(unittest.TestCase):
         #xdot
         xderivative_array = np.array(derivatives[0,:]) #Finite Differences Derivatives
         x_dot_function = -1 * b * x_array * y_array #Solution results plugged into differential equation
-        xdifference = x_array - xderivative_array #Difference should be zero
+        xdifference = x_dot_function - xderivative_array #Difference should be zero
         
         for i in range(xdifference.shape[0]): #For all solution points
-            self.assertTrue(np.abs(xdifference[i]) < .005) #Test checks if absolute difference is close enough to 0
+            self.assertTrue(np.abs(xdifference[i]) < .05) #Test checks if absolute difference is close enough to 0
            
         #ydot
         yderivative_array = np.array(derivatives[1,:]) #Finite Differences Derivatives
         y_dot_function = b * x_array * y_array - k * y_array #Solution results plugged into differential equation
-        ydifference = y_array - yderivative_array #Difference should be zero
+        ydifference = y_dot_function - yderivative_array #Difference should be zero
         
         for i in range(ydifference.shape[0]): #For all solution points
-            self.assertTrue(np.abs(ydifference[i]) < .005) #Test checks if absolute difference is close enough to 0
+            self.assertTrue(np.abs(ydifference[i]) < .05) #Test checks if absolute difference is close enough to 0
             
         #zdot
         zderivative_array = np.array(derivatives[2,:]) #Finite Differences Derivatives
         z_dot_function = k * y_array #Solution results plugged into differential equation
-        zdifference = z_array - zderivative_array #Difference should be zero
+        zdifference = z_dot_function - zderivative_array #Difference should be zero
         
         for i in range(zdifference.shape[0]): #For all solution points
-            self.assertTrue(np.abs(zdifference[i]) < .005) #Test checks if absolute difference is close enough to 0    
+            self.assertTrue(np.abs(zdifference[i]) < .05) #Test checks if absolute difference is close enough to 0    
             
     def test_sums(self):
         '''
@@ -77,7 +77,7 @@ class TestODEs(unittest.TestCase):
         '''
         h = 600 #Time Step - days
         k = 2*h #Time Step - half days
-        y_init = np.array([0.99999, 0.00001, 0]) #Initial Condition
+        SIR_init = np.array([0.99999, 0.00001, 0]) #Initial Condition
         tspan = np.linspace(0, 600, k + 2) #600 days, halfday-steps + 1 extra
         
         b = .30 #Infection Rate
@@ -92,9 +92,11 @@ class TestODEs(unittest.TestCase):
         z_array = np.array(sol.y[2,:-1]) #Make these np arrays for element-wise addition
             
         sum_array = x_array + y_array + z_array #This should equal one
-        ones = np.ones(sum_array(shape)) #Construct actual ones
+        ones = np.ones(sum_array.shape) #Construct actual ones
         diff = ones - sum_array #This should be close to zero
-        self.assertTrue(np.abs(np.maximum(diff)) < .005 ) #We test to see if this is close enough to zero
+        
+        for i in range(diff.shape[0]):
+            self.assertTrue(np.abs(diff[i]) < .005 ) #We test to see if this is close enough to zero
 
     def test_derivative_sums(self): 
         '''
@@ -102,7 +104,7 @@ class TestODEs(unittest.TestCase):
         '''
         h = 600 #Time Step - days
         k = 2*h #Time Step - half days
-        y_init = np.array([0.99999, 0.00001, 0]) #Initial Condition
+        SIR_init = np.array([0.99999, 0.00001, 0]) #Initial Condition
         tspan = np.linspace(0, 600, k + 2) #600 days, halfday-steps + 1 extra
         
         b = .30 #Infection Rate
@@ -139,4 +141,6 @@ class TestODEs(unittest.TestCase):
         zderivative_array = np.array(derivatives[2,:]) #Finite Differences Derivatives
         
         sum_array = xderivative_array + yderivative_array + zderivative_array #This should be close to zero
-        self.assertTrue(np.abs(np.maximum(sum_array)) < .005 ) #We test to see if this is close enough to zero
+        
+        for i in range(sum_array.shape[0]):
+            self.assertTrue(np.abs(sum_array[i]) < .005 ) #We test to see if this is close enough to zero
